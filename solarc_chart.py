e5791c4fd4dfd83ccd249a6329e4d90bd5af8d6a
LRITA:D <148297794+kukuxiix@users.noreply.github.com>
2023-11-02 22:12:48 +1100

    Solar PV table (use streamlit run)

--- solarc_chart.py
+++ solarc_chart.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import plotly.graph_objects as go
 import pandas as pd
 
+
 #Solar panel costs (table)
 def scrape_solar_prices(url):
     response = requests.get(url)
@@ -20,6 +21,7 @@ def scrape_solar_prices(url):
     
     return headers, data
 
+
 #Average commercial solar panel costs per watt (one number)
 def scrape_average_price(url):
     response = requests.get(url)
@@ -30,6 +32,7 @@ def scrape_average_price(url):
     
     return average_price
 
+
 def main():
     st.title('Solar Panel Costs')
 
@@ -37,10 +40,9 @@ def main():
     color_one = "#FF4500" 
     color_two = "#FFD700"  
     color_three = "#FFFAF0" 
-    
     url = "https://www.solarchoice.net.au/solar-panels/solar-power-system-prices"
     
-    # Display average commercial cost per watt
+    #Display average commercial cost per watt
     try:
         average_price = scrape_average_price(url)
         st.markdown(f"### **Average Commercial Cost per Watt**", unsafe_allow_html=True)
@@ -48,10 +50,10 @@ def main():
     except Exception as e:
         st.error(f"An error occurred: {e}")
 
-    # Display the table title and adjust spacing
+    #Display the table title and adjust spacing
     st.markdown("<h4 style='margin-bottom:5px;margin-top:5px;'>Solar Panel Cost by Region and kW</h4>", unsafe_allow_html=True)
 
-    # Fetch and display table
+    #Fetch and display table
     try:
         headers, data = scrape_solar_prices(url)
         transposed_data = [list(item) for item in zip(*data)] 
@@ -62,12 +64,12 @@ def main():
                 fill_color=color_two,
                 align='center',
                 font=dict(color='black', size=14),
-                height=30  # Adjusted height for header
+                height=30  #Height for header
             ),
             cells=dict(
                 values=transposed_data,  
                 line_color=color_one,
-                fill=dict(color=[color_two, color_three]),  # Alternating row colors
+                fill=dict(color=[color_two, color_three]),  #Row colors
                 align='center',
                 font=dict(color='black', size=13)
             ))
@@ -75,7 +77,7 @@ def main():
 
         fig.update_layout(width=1000, height=600, margin=dict(t=5, b=0, l=0, r=0))
         
-        # Using Streamlit to display the chart
+        #Using Streamlit to display the chart
         st.plotly_chart(fig, use_container_width=True)
     except Exception as e:
         st.error(f"An error occurred: {e}")
